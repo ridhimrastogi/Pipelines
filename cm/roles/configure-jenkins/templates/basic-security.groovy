4ec9a91cc609cb0b78ffc9e12ee6672a1b8a6146
Ridhim Rastogi <rrastog3@ncsu.edu>
2020-02-29 13:24:07 -0500

    Fix: Automate login into jenkins after installation

--- cm/roles/configure-jenkins/templates/basic-security.groovy
+++ cm/roles/configure-jenkins/templates/basic-security.groovy
@@ -2,17 +2,23 @@
 
 import jenkins.model.*
 import hudson.security.*
+import jenkins.install.InstallState
 
 def instance = Jenkins.getInstance()
 
 println "--> creating local user 'admin'"
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount('admin','admin')
 instance.setSecurityRealm(hudsonRealm)
+hudsonRealm.createAccount('admin','admin')
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
 instance.setCrumbIssuer(null)
-instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+
+if (!instance.installState.isSetupComplete()) {
+  println '--> Neutering SetupWizard'
+  InstallState.INITIAL_SETUP_COMPLETED.initializeState()
+}
+
 instance.save()
