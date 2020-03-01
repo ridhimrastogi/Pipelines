c06b50a9e03f4aeb55db9803c25521eff2e7dfc1
Ridhim Rastogi <rrastog3@ncsu.edu>
2020-02-29 22:39:14 -0500

    Add check for specified project in build command

--- commands/build.js
+++ commands/build.js
@@ -28,10 +28,16 @@ async function run(project) {
 
     // the paths should be from root of cm directory
     // Transforming path of the files in host to the path in VM's shared folder
-    let filePath = '/bakerx/cm/checkbox.yaml';
+    if( project == 'checkbox.io'){
+        let filePath = '/bakerx/cm/checkbox.yaml';
     let inventorypath = '/bakerx/cm/jenkins_jobs.ini'
     console.log(chalk.blueBright(`Building ${project}...`));
     let result = sshSync(`/bakerx/cm/build-checkbox.sh ${filePath} ${inventorypath}`, 'vagrant@192.168.33.20');
     if( result.error ) { process.exit( result.status ); }
+    } else {
+        console.log("Not yet supported. Try again with checkbox.io")
+    }
+
+
 
 }
